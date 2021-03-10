aaf979a9e9fc2d957a68b14135695dafef3bc112
UzheNeKate <eavrub@gmail.com>
2021-03-10 19:55:49 +0300

    cloud connector added

--- src/main/java/request/CloudConnector.java
+++ src/main/java/request/CloudConnector.java
@@ -1,10 +1,33 @@
 package request;
 
-public class CloudConnector {
-    static public String  getFromCloud(String key){
-        //TODO: перенести из другого класса
+import Data.AbstractWeatherInfo;
+import parser.JsonParser;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+public class CloudConnector<T extends AbstractWeatherInfo> {
+
+    private final static String API_URL_FORMAT = System.getenv("API_URL_FORMAT");
+    private final static String ACCESS_KEY = System.getenv("ACCESS_KEY");
+
+    public HttpConnectionInfo  getFromCloud(String city, Type type){
+        HttpURLConnection con;
+        try {
+            URL url = new URL(String.format(API_URL_FORMAT, ACCESS_KEY, city));
+            con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod(Request.GET.toString());
+            JsonParser<T> parser = new JsonParser<T>();
+            T info = parser.getFromJson(new String(con.getInputStream().readAllBytes(), StandardCharsets.UTF_8), type);
+            return new HttpConnectionInfo(200,  " OK ", info);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new HttpConnectionInfo(400, "Bad Request", null);
+        }
         //opens a connection with the cloud server
         //returns received string
-        return null;
     }
 }
--- src/main/java/request/WeatherRequestHandler.java
+++ src/main/java/request/WeatherRequestHandler.java
@@ -1,31 +1,10 @@
 package request;
 
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
 
 public class WeatherRequestHandler implements RequestHandler {
-    private final static String API_URL_FORMAT = System.getenv("API_URL_FORMAT");
-    private final static String ACCESS_KEY = System.getenv("ACCESS_KEY");
 
     @Override
     public String handle(String request) {
-        String weatherRequestFormat = "/weather?city=";
-        if (request.startsWith(weatherRequestFormat)) {
-            var city = request.substring(weatherRequestFormat.length());
-            HttpURLConnection con;
-            try {
-                URL url = new URL(String.format(API_URL_FORMAT, ACCESS_KEY, city));
-                con = (HttpURLConnection) url.openConnection();
-                con.setRequestMethod(Request.GET.toString());
-                return "200 OK " + new String(con.getInputStream().readAllBytes(), StandardCharsets.UTF_8);
-            } catch (IOException e) {
-                e.printStackTrace();
-                return new UndefinedRequestHandler().handle(request);
-            }
-        } else {
-            return "400 Bad Request";
-        }
+        return null;
     }
 }
