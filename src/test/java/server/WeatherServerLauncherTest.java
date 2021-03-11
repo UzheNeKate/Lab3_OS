26627d2d0012402e4b4f306d6af35b8457e15bca
UzheNeKate <eavrub@gmail.com>
2021-03-11 23:11:36 +0300

    something like integration test added

--- src/test/java/server/WeatherServerLauncherTest.java
+++ src/test/java/server/WeatherServerLauncherTest.java
@@ -4,7 +4,11 @@ import data.HumidityInfo;
 import org.junit.jupiter.api.Test;
 import parser.JsonParser;
 
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Arrays;
+import java.util.Random;
 
 class WeatherServerLauncherTest {
     @Test
@@ -24,6 +28,48 @@ class WeatherServerLauncherTest {
         var parts = part.split("[/?=&]");
         System.out.println(parts.length);
         Arrays.stream(parts).forEach(System.out::println);
+    }
+
+    static String[] requests = {"weather", "humidity", "wrong"};
+    static String[] cities = {"Minsk", "New York", "Brest", "Moscow", "London", "Warsaw",
+            "Kiev", "Moscow", "Berlin", "Rome", "Paris"};
+
+    static String getRequest(){
+        Random rnd = new Random();
+        return String.format("%s?city=%s", requests[rnd.nextInt(requests.length - 1)],
+                cities[rnd.nextInt(cities.length - 1)]);
+    }
+
+    @Test
+    public static void main(String[] args) {
+        if (args.length < 1) return;
+
+        String domainName = args[0];
+
+        String hostname = "localhost";
+        int port = 8080;
+
+        try (Socket socket = new Socket(hostname, port)) {
+            InputStream input = socket.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+            String line;
+
+            OutputStream output = socket.getOutputStream();
+            PrintWriter writer = new PrintWriter(output, true);
+            for (int i = 0; i < 5; i++)
+                writer.println(getRequest());
+
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (UnknownHostException ex) {
+
+            System.out.println("Server not found: " + ex.getMessage());
+
+        } catch (IOException ex) {
+
+            System.out.println("I/O error: " + ex.getMessage());
+        }
     }
 
 }
